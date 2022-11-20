ea9f62178e82a14f7b722446ebfa720e8e245b76
Nocturna <90935456+bobb606@users.noreply.github.com>
2022-11-20 15:38:54 -0800

    Add files via upload

---
+++ esp.lua
@@ -0,0 +1,3 @@
+-- Made by Blissful#4992 (Auto Updates)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Cornyllius/ESPs/main/BlissESP.lua", true))()
---
+++ octo.lua
@@ -0,0 +1,2 @@
+_G.wl_key = '0a1a9c0ef27de7d401706cf66faf00bbf92599e3'
+loadstring(game:HttpGet('https://www.octohook.xyz/script/loader.lua'))()
